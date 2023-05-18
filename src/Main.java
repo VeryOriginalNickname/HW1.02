a40d3719d64048e24e9eb2a90973ea403641c9fd
Dmitry Kurganov <dimacup30@gmail.com>
2023-05-18 22:18:55 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int account = 100;
+        int payment = 1100;
+        int bonus = 0;
+
+        if (payment>1000) {bonus = payment/100;}
+
+        int end = account + payment + bonus;
+
+        System.out.println("После произведения оплаты в " + payment + " рублей, на счету " + end + " рублей.");
+
+    }
+}
